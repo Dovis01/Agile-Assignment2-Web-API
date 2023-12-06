6826b1c84b956c666834473f313b4482440f9fa6
Shijin Zhang <1364226740@qq.com>
2023-12-07 07:06:40 +0800

    Initial structure

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authenticate from './authenticate';
+import moviesRouter from './api/movies';
+import cors from 'cors';
+import usersRouter from './api/users';
+import './seedData'
+import './db';
+import defaultErrHandler from './errHandler';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/users', usersRouter);
+//app.use('/api/movies', authenticate,  moviesRouter);
+app.use('/api/movies', moviesRouter);
+app.use(defaultErrHandler);
+
+let server = app.listen(port, () => {
+    console.info(`Server running at ${port}`);
+});
+module.exports = server
---
+++ seedData/index.js
@@ -0,0 +1,37 @@
+import userModel from '../api/users/userModel';
+import users from '../initialise-dev/users';
+import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from '../initialise-dev/movies';
+
+dotenv.config();
+
+// deletes all user documents in collection and inserts test data
+async function loadUsers() {
+    console.log('load user Data');
+    try {
+        await userModel.deleteMany();
+        await users.forEach(user => userModel.create(user));
+        console.info(`${users.length} users were successfully stored.`);
+    } catch (err) {
+        console.error(`failed to Load user Data: ${err}`);
+    }
+}
+
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+    console.log('load seed data');
+    console.log(movies.length);
+    try {
+        await movieModel.deleteMany();
+        await movieModel.collection.insertMany(movies);
+        console.info(`${movies.length} Movies were successfully stored.`);
+    } catch (err) {
+        console.error(`failed to Load movie Data: ${err}`);
+    }
+}
+
+if (process.env.NODE_ENV === 'development') {
+    loadUsers();
+    loadMovies();//ADD THIS LINE
+}
