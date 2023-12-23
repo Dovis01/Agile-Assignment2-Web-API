2ed23a89c82161f4edb0492358944f8f4b8a71df
Shijin Zhang <1364226740@qq.com>
2023-12-24 05:28:40 +0800

    Add-more server API router paths about movies endpoint

--- api/movies/index.js
+++ api/movies/index.js
@@ -1,12 +1,18 @@
 import movieModel from './movieModel';
-import {getGenres, getUpcomingMovies} from '../tmdb-api';
+import {
+    getDiscoverMovies, getMovieReviews, getMovieVideos,
+    getGenres, getMovie, getMovieCredits, getMovieImages,
+    getMovieRecommendations, getNowPlayingMovies,
+    getUpcomingMovies, getWeekTrendingMovies
+} from '../tmdb-api';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 
 const router = express.Router();
 
+//Get all movies data from database storage
 router.get('/', asyncHandler(async (req, res) => {
-    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    let {page = 1, limit = 10} = req.query; // destructure page and limit and set default values
     [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
 
     // Parallel execution of counting movies and getting movies using movieModel
@@ -26,25 +32,180 @@ router.get('/', asyncHandler(async (req, res) => {
     res.status(200).json(returnObject);
 }));
 
-// Get movie details
+// Get a specific movie details from database storage
 router.get('/:id', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
     const movie = await movieModel.findByMovieDBId(id);
     if (movie) {
         res.status(200).json(movie);
     } else {
-        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+        res.status(404).json({message: 'The movie you requested could not be found.', code: 404});
+    }
+}));
+
+//From TMDB API
+router.get('/tmdb/discover', asyncHandler(async (req, res) => {
+    try {
+        const page = req.query.page;
+        const validation = validatePageNumber(page, 250);
+        if (!validation.isValid) {
+            return res.status(validation.status).json(validation.json);
+        }
+        const discoverMovies = await getDiscoverMovies(page);
+        res.status(200).json(discoverMovies);
+    } catch (error) {
+        res.status(500).json({success: false, message: 'Internal server error.', code: 500});
     }
 }));
 
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
-    const upcomingMovies = await getUpcomingMovies();
-    res.status(200).json(upcomingMovies);
+    try {
+        const page = req.query.page;
+        const validation = validatePageNumber(page, 14);
+        if (!validation.isValid) {
+            return res.status(validation.status).json(validation.json);
+        }
+        const upcomingMovies = await getUpcomingMovies(page);
+        res.status(200).json(upcomingMovies);
+    } catch (error) {
+        res.status(500).json({success: false, message: 'Internal server error.', code: 500});
+    }
+}));
+
+router.get('/tmdb/nowplaying', asyncHandler(async (req, res) => {
+    try {
+        const page = req.query.page;
+        const validation = validatePageNumber(page, 51);
+        if (!validation.isValid) {
+            return res.status(validation.status).json(validation.json);
+        }
+        const nowPlayingMovies = await getNowPlayingMovies(page);
+        res.status(200).json(nowPlayingMovies);
+    } catch (error) {
+        res.status(500).json({success: false, message: 'Internal server error.', code: 500});
+    }
+}));
+
+router.get('/tmdb/week_trending', asyncHandler(async (req, res) => {
+    try {
+        const page = req.query.page;
+        const validation = validatePageNumber(page, 100);
+        if (!validation.isValid) {
+            return res.status(validation.status).json(validation.json);
+        }
+        const weekTrendingMovies = await getWeekTrendingMovies(page);
+        res.status(200).json(weekTrendingMovies);
+    } catch (error) {
+        res.status(500).json({success: false, message: 'Internal server error.', code: 500});
+    }
+}));
+
+router.get('/tmdb/:id/recommendations', asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {page} = req.query;
+        const validation = validatePageNumber(page, 1);
+        if (!validation.isValid) {
+            return res.status(validation.status).json(validation.json);
+        }
+        const recommendationsMoviesList = await getMovieRecommendations(id, page);
+        res.status(200).json(recommendationsMoviesList);
+    } catch (error) {
+        res.status(404).json({success:false, message: 'The recommendations data could not be found for this movie id you requested.', code: 404});
+    }
 }));
 
 router.get('/tmdb/genres', asyncHandler(async (req, res) => {
     const genresList = await getGenres();
     res.status(200).json(genresList);
 }));
+
+router.get('/tmdb/:id/images', asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const imagesList = await getMovieImages(id);
+        res.status(200).json(imagesList);
+    } catch (error) {
+        res.status(404).json({success:false, message: 'The images data could not be found for this movie id you requested.', code: 404});
+    }
+}));
+
+router.get('/tmdb/:id/videos', asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const videosList = await getMovieVideos(id);
+        res.status(200).json(videosList);
+    } catch (error) {
+        res.status(404).json({success:false, message: 'The videos data could not be found for this movie id you requested.', code: 404});
+    }
+}));
+
+router.get('/tmdb/:id/credits', asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const creditsList = await getMovieCredits(id);
+        res.status(200).json(creditsList);
+    } catch (error) {
+        res.status(404).json({success:false, message: 'The movie related credits data could not be found for this movie id you requested.', code: 404});
+    }
+}));
+
+router.get('/tmdb/:id/reviews', asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const reviewsList = await getMovieReviews(id);
+        res.status(200).json(reviewsList);
+    } catch (error) {
+        res.status(404).json({success:false, message: 'The reviews data could not be found for this movie id you requested.', code: 404});
+    }
+}));
+
+router.get('/tmdb/:id', asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const movie = await getMovie(id);
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(404).json({success:false, message: 'The movie details data could not be found for this movie id you requested.', code: 404});
+    }
+}));
+
+function validatePageNumber(page, maxValidPage) {
+    const pageNumber = parseInt(page, 10);
+    if (isNaN(pageNumber)) {
+        return {
+            isValid: false,
+            status: 400,
+            json: {
+                success: false,
+                message: 'Page number must be a number.',
+                code: 400
+            }
+        };
+    }
+    if (pageNumber < 1) {
+        return {
+            isValid: false,
+            status: 400,
+            json: {
+                success: false,
+                message: `Page number cannot be less than 1.`,
+                code: 400
+            }
+        };
+    }
+    if (pageNumber > maxValidPage) {
+        return {
+            isValid: false,
+            status: 400,
+            json: {
+                success: false,
+                message: `Page number cannot be greater than ${maxValidPage}.`,
+                code: 400
+            }
+        };
+    }
+    return {isValid: true};
+}
 
 export default router;
--- api/tmdb-api.js
+++ api/tmdb-api.js
@@ -1,9 +1,175 @@
 import fetch from 'node-fetch';
 
-export const getUpcomingMovies = async () => {
+export const getDiscoverMovies = (page) => {
+    const pageLast = page * 2;
+    const pageFirst = pageLast - 1;
+
+    return Promise.all([
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageFirst}`),
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageLast}`)
+    ])
+        .then(responses => {
+            for (const response of responses) {
+                if (!response.ok) {
+                    throw new Error('Problem fetching movies');
+                }
+            }
+            return Promise.all(responses.map(response => response.json()));
+        })
+        .then(data => {
+            const moviesPage1 = data[0].results || [];
+            const moviesPage2 = data[1].results || [];
+            const combinedMovies = [...moviesPage1, ...moviesPage2];
+
+            return {
+                page: page,
+                results: combinedMovies,
+                total_results: 10000,
+                total_pages: 250
+            };
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
+export const getUpcomingMovies = (page) => {
+    const pageLast = page * 2;
+    const pageFirst = pageLast - 1;
+
+    return Promise.all([
+        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${pageFirst}`),
+        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${pageLast}`)
+    ])
+        .then(responses => {
+            for (const response of responses) {
+                if (!response.ok) {
+                    throw new Error('Problem fetching movies');
+                }
+            }
+            return Promise.all(responses.map(response => response.json()));
+        })
+        .then(data => {
+            const moviesPage1 = data[0].results || [];
+            const moviesPage2 = data[1].results || [];
+            const combinedMovies = [...moviesPage1, ...moviesPage2];
+
+            return {
+                page: page,
+                results: combinedMovies,
+                total_results: 537,
+                total_pages: 14
+            };
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
+
+export const getNowPlayingMovies = (page) => {
+    const pageLast = page * 2;
+    const pageFirst = pageLast - 1;
+
+    return Promise.all([
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${pageFirst}`),
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${pageLast}`)
+    ])
+        .then(responses => {
+            for (const response of responses) {
+                if (!response.ok) {
+                    throw new Error('Problem fetching movies');
+                }
+            }
+            return Promise.all(responses.map(response => response.json()));
+        })
+        .then(data => {
+            const moviesPage1 = data[0].results || [];
+            const moviesPage2 = data[1].results || [];
+            const combinedMovies = [...moviesPage1, ...moviesPage2];
+
+            return {
+                page: page,
+                results: combinedMovies,
+                total_results: 2003,
+                total_pages: 51
+            };
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
+export const getWeekTrendingMovies = (page) => {
+    const pageLast = page * 2;
+    const pageFirst = pageLast - 1;
+
+    return Promise.all([
+        fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageFirst}`),
+        fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageLast}`)
+    ])
+        .then(responses => {
+            for (const response of responses) {
+                if (!response.ok) {
+                    throw new Error('Problem fetching movies');
+                }
+            }
+            return Promise.all(responses.map(response => response.json()));
+        })
+        .then(data => {
+            const moviesPage1 = data[0].results || [];
+            const moviesPage2 = data[1].results || [];
+            const combinedMovies = [...moviesPage1, ...moviesPage2];
+
+            return {
+                page: page,
+                results: combinedMovies,
+                total_results: 4000,
+                total_pages: 100
+            };
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
+export const getMovieRecommendations = (id,page) => {
+    const pageLast = page * 2;
+    const pageFirst = pageLast - 1;
+
+    return Promise.all([
+        fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageFirst}`),
+        fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageLast}`)
+    ])
+        .then(responses => {
+            for (const response of responses) {
+                if (!response.ok) {
+                    throw new Error('Problem fetching movies');
+                }
+            }
+            return Promise.all(responses.map(response => response.json()));
+        })
+        .then(data => {
+            const moviesPage1 = data[0].results || [];
+            const moviesPage2 = data[1].results || [];
+            const combinedMovies = [...moviesPage1, ...moviesPage2];
+
+            return {
+                page: page,
+                results: combinedMovies,
+                total_results: data[0].total_results,
+                total_pages: Math.ceil(data[0].total_results/40)
+            };
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
+export const getGenres = async () => {
     try {
         const response = await fetch(
-            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
         );
 
         if (!response.ok) {
@@ -16,19 +182,77 @@ export const getUpcomingMovies = async () => {
     }
 };
 
-export const getGenres = async () => {
-    try {
-        const response = await fetch(
-            `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
-        );
-
+export const getMovie = (id) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
         if (!response.ok) {
             throw new Error(response.json().message);
         }
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
+};
 
-        return await response.json();
-    } catch (error) {
-        throw error;
-    }
+export const getMovieImages = (id) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+
+    })
+        .catch((error) => {
+            throw error
+        });
+};
+
+
+export const getMovieVideos = (id) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+
+    })
+        .catch((error) => {
+            throw error
+        });
+};
+
+export const getMovieCredits = (id) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+
+    })
+        .catch((error) => {
+            throw error
+        });
+};
+
+export const getMovieReviews = (id) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+
+    }).catch((error) => {
+        throw error
+    });
 };
 
