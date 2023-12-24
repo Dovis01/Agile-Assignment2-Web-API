2cb3324260fa201ea1a1fd03ef5100e4e6b07519
Shijin Zhang <1364226740@qq.com>
2023-12-24 08:39:02 +0800

    Add-initial data about people in seed data and upload it

--- seedData/index.js
+++ seedData/index.js
@@ -3,12 +3,14 @@ import users from './users';
 import dotenv from 'dotenv';
 import movieModel from '../api/movies/movieModel';
 import movies from './movies';
+import peopleModel from '../api/people/peopleModel';
+import people from './people';
 
 dotenv.config();
 
 // deletes all user documents in collection and inserts test data
 async function loadUsers() {
-    console.log('load user Data');
+    console.log('load users Data');
     try {
         await userModel.deleteMany();
         await users.forEach(user => userModel.create(user));
@@ -20,8 +22,7 @@ async function loadUsers() {
 
 // deletes all movies documents in collection and inserts test data
 export async function loadMovies() {
-    console.log('load seed data');
-    console.log(movies.length);
+    console.log('load movies data');
     try {
         await movieModel.deleteMany();
         await movieModel.collection.insertMany(movies);
@@ -31,7 +32,20 @@ export async function loadMovies() {
     }
 }
 
+// deletes all people documents in collection and inserts test data
+export async function loadPeople() {
+    console.log('load people data');
+    try {
+        await peopleModel.deleteMany();
+        await peopleModel.collection.insertMany(people);
+        console.info(`${people.length} People were successfully stored.`);
+    } catch (err) {
+        console.error(`failed to Load people Data: ${err}`);
+    }
+}
+
 if (process.env.NODE_ENV === 'development') {
     loadUsers();
     loadMovies();//ADD THIS LINE
+    loadPeople();
 }
