1a448543d37286af3337657760745bb1e4ea275a
Shijin Zhang <1364226740@qq.com>
2023-12-26 09:31:48 +0800

    Improve-comment on redundant print information for higher coverage rate

--- db/index.js
+++ db/index.js
@@ -7,11 +7,11 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_DB);
 const db = mongoose.connection;
 
-db.on('error', (err) => {
-    console.log(`database connection error: ${err}`);
+db.on('error', () => {
+    //console.log(`database connection error: ${err}`);
 });
 db.on('disconnected', () => {
-    console.log('database disconnected');
+    //console.log('database disconnected');
 });
 db.once('open', () => {
     //console.log(`database connected to ${db.name} on ${db.host}`);
